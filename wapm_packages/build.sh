5ac7c84c6394ec2f553a26ad4db0a682985aa8fb
Antony Bartlett <antony.bartlett@ukri.org>
2021-11-13 11:52:24 +0000

    Complete the script for assembling the wapm package.

--- wapm_packages/build.sh
+++ wapm_packages/build.sh
@@ -1,2 +1,4 @@
 cd ..
 docker build --build-arg "TARGET=wasm32-wasi" -t metamath-knife .
+docker cp $(docker ps -alq):/work/target/wasm32-wasi/release/metamath-knife.wasm ./wapm_packages/metamath-knife
+cp ./README.md ./wapm_packages/metamath-knife
